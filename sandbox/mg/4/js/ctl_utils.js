17366cae350f4f1f5eb67cff279cd97da117e189
Mario Dentone <mario.dentone@gmail.com>
2017-03-12 17:56:34 +0100

    test 6 sorcerer fix 2

--- sandbox/mg/4/js/ctl_utils.js
+++ sandbox/mg/4/js/ctl_utils.js
@@ -12,10 +12,22 @@ function trace(szMsg){
     console.log(szMsg);
 }
 
+/* mat:
 $(window).ready(function() {
 	sizeHandler();
 
 });
+ */
+
+function isIOS() {
+    var iDevices = ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'];
+    while (iDevices.length) {
+        if (navigator.platform === iDevices.pop()) {
+            return true;
+        }
+    }
+    return false;
+}
 
 window.addEventListener("orientationchange", onOrientationChange );
 
