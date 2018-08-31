90019b66d0a949f1a6792e47f1e2e44afe478a33
mdentone <mario.dentone@gmail.com>
2018-08-31 20:12:17 +0200

    Create _.js

---
+++ m/_.js
@@ -0,0 +1,9 @@
+
+// Copyright (c) MDE. All rights reserved.
+
+"use strict";
+
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+window.m = { }; // m is for app code
+window.x = { }; // x is for m's core
