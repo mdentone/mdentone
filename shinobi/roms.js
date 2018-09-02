3a2b4c61e17185f83ad181bcf6cbe689b14e99d2
mdentone <mario.dentone@gmail.com>
2018-09-02 19:43:36 +0200

    Add files via upload

--- shinobi/roms.js
+++ shinobi/roms.js
@@ -1,4 +1,7 @@
-var RomList = [
-    "AlexKidd.asm",
-    "Sonic.asm"
+
+// Copyright (c) MDE. All rights reserved.
+
+var roms = [
+    { file: "AlexKidd.asm", title: "Alex Kidd in Miracle World" },
+    { file: "Sonic.asm", title: "Sonic the Hedgehog" }
 ];
