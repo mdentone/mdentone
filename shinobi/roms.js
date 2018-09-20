e32ab9c0c7c876abd84d6c8732443fe5560806cf
mdentone <mario.dentone@gmail.com>
2018-09-20 20:44:01 +0200

    prototype cleanup

--- shinobi/roms.js
+++
@@ -1,8 +0,0 @@
-
-// Copyright (c) MDE. All rights reserved.
-
-var roms = [
-    { file: "alexkidd.asm", title: "Alex Kidd in Miracle World" },
-    { file: "mickey.asm", title: "Mickey Mouse - Castle of Illusion" },
-    { file: "sonic.asm", title: "Sonic the Hedgehog" }
-];
---
+++ shinobi/roms/roms.js
@@ -0,0 +1,8 @@
+
+// Copyright (c) MDE. All rights reserved.
+
+var roms = [
+    { file: "alexkidd.asm", title: "Alex Kidd in Miracle World" },
+    { file: "mickey.asm", title: "Mickey Mouse - Castle of Illusion" },
+    { file: "sonic.asm", title: "Sonic the Hedgehog" }
+];
