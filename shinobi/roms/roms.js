4b30d6708fa59f18fa159fb02ec1015190ca6aff
mdentone <mario.dentone@gmail.com>
2018-10-04 17:31:57 +0200

    Add files via upload

--- shinobi/roms/roms.js
+++ shinobi/roms/roms.js
@@ -1,8 +1,13 @@
 
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 // Copyright (c) MDE. All rights reserved.
 
+//::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 var roms = [
-    { file: "alexkidd.asm", title: "Alex Kidd in Miracle World" },
-    { file: "mickey.asm", title: "Mickey Mouse - Castle of Illusion" },
-    { file: "sonic.asm", title: "Sonic the Hedgehog" }
+    { file: "alexkidd.asm"  , title: "Alex Kidd in Miracle World" },
+    { file: "bubble.asm"    , title: "Bubble Bobble" },
+    { file: "mickey.asm"    , title: "Mickey Mouse - Castle of Illusion" },
+    { file: "sonic.asm"     , title: "Sonic the Hedgehog" }
 ];
